4d87d62aa6ce4cdb39f11b616db6d868b0df907c
jibedoubleve <jibedoubleve@users.noreply.github.com>
2026-04-03 16:04:41 +0200

    (fix) Add some dummy code

--- src/program.cs
+++ src/program.cs
@@ -6,3 +6,5 @@ Console.WriteLine("Hello, World!");
 // TODO: Bug: This code does not compile because of the missing closing parenthesis in the Console.WriteLine statement.
 Console.WriteLine("This is a bug fix example.");
 
+// todo: Wrong text in Console.WriteLine statement.
+Console.WriteLine("This is a wrong text example.");
