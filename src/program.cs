1324069008c1779fff50185bc76aaab98ee8bdd7
jibedoubleve <jibedoubleve@users.noreply.github.com>
2026-04-03 14:47:32 +0200

    (feat) Add some code with todo

---
+++ src/program.cs
@@ -0,0 +1,9 @@
+// Todo create a new ticket for this file and move the code there
+using System;
+
+Console.WriteLine("Hello, World!");
+
+// TODO: Create another Console.WriteLine with your name
+// todo: Create a new method that adds two numbers and returns the result
+
+/* TODO: Wrfite a method that takes a string as input and returns the number of characters in the string. */
