d0bfe36c80e21283cc459621726dfae580788a1f
jibedoubleve <jibedoubleve@users.noreply.github.com>
2026-04-03 15:19:54 +0200

    (fix) Add trigger fot the new todo

--- src/program.cs
+++ src/program.cs
@@ -1,9 +1,4 @@
-// Todo create a new ticket for this file and move the code there
 using System;
 
+// TODO: add new code here
 Console.WriteLine("Hello, World!");
-
-// TODO: Create another Console.WriteLine with your name
-// todo: Create a new method that adds two numbers and returns the result
-
-/* TODO: Wrfite a method that takes a string as input and returns the number of characters in the string. */
